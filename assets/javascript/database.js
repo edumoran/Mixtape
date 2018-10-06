8be031056451ae35ff9b854e0d0a059fb11223dd
Jorge Baez <jibm_82@yahoo.com.mx>
2018-10-06 14:25:22 -0500

    Implemented configuration persistence and playlist information retrieval

---
+++ assets/javascript/database.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBf5gfg9sP0TMfjuJZuTjpZ5-hXimvCjdY",
+  authDomain: "manhattan-f7fb3.firebaseapp.com",
+  databaseURL: "https://manhattan-f7fb3.firebaseio.com",
+  projectId: "manhattan-f7fb3",
+  storageBucket: "manhattan-f7fb3.appspot.com",
+  messagingSenderId: "1001136473148"
+};
+firebase.initializeApp(firebaseConfig);
